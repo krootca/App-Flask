3582d130d9c6b01e29470b549dd5665dafc49e3b
krootca <63897401+krootca@users.noreply.github.com>
2021-10-27 18:38:37 -0300

    ok
    all is ok

--- database.py
+++ database.py
@@ -17,9 +17,9 @@ tel_peru_re	= re.compile(r"^.+(51)([0-9\w]{9})$")
 direction_re= re.compile(r"^[a-zA-Z0-9]{3,30}$")
 #---------
 #-------------CREATE ALL DB----------
-def createDB():
-	db_app = sqlite3.connect("database.db", check_same_thread=False)
-	cursor = db_app.cursor()
+def createDB(): #Crea la primera base de datos
+	db_app = sqlite3.connect("database.db", check_same_thread=False) #Crea un archivo database.db
+	cursor = db_app.cursor() #sirve para crear algoritmos de bases de datos
 	cursor.execute("""CREATE TABLE "accounts" (
 						"user_name"	TEXT NOT NULL,
 						"user_surname"	TEXT NOT NULL,
@@ -30,10 +30,10 @@ def createDB():
 						"age"	TEXT NOT NULL,
 						"user_id"	INT NOT NULL UNIQUE);""")
 
-	db_app.commit()	
+	db_app.commit()	 # guarda lo creado
 	db_app.close()
 
-def computerDB():
+def computerDB(): #lo mismo que arriba pero con otros datos para crear
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	cursor.execute("""CREATE TABLE "computerDB" (
@@ -105,7 +105,7 @@ def imagesDB():
 	db_app.commit()	
 	db_app.close()
 
-def createAllDB():
+def createAllDB(): # esta funcion solo crea todas las bases de datos
 
 	createDB()
 	computerDB()
@@ -113,21 +113,21 @@ def createAllDB():
 	incidentsDB()
 	imagesDB()
 #------------------------------------------------------------
-try:
-	createAllDB()
+try: # intenta crear las bases de datos, pero si no lo logra no hace nada, sirve para que al momento de correr el archivo, cree la base de datos, si ocurre un error, no dice nada
+	createAllDB() 
 except Exception as e:
 	pass
 
-def return_all():
+def return_all(): #retorna todos los datos de la base de datos
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	try:
 		datos=cursor.execute('SELECT * FROM accounts').fetchall()
 		db_app.close()
-		return datos
-	except Exception as e:
+		return datos #aqui retorna todo
+	except Exception as e: # si no funciona no hace nada
 		pass
-def account_login(email, password): #true is ok # false is error with regular expretion # None is login failed
+def account_login(email, password): #sirve para logearse, si el password y el email existe, retorna true, si no, retorna false
 	for i in return_all():
 		if str(email_re.search(email))!="None" and str(password_re.search(password))!="None":
 			if email==i[2] and password == i[3]: #if email is a email
@@ -135,11 +135,11 @@ def account_login(email, password): #true is ok # false is error with regular ex
 		else: #email or password is incorrect
 			return False 
 
-def cityVerific(direction):
+def cityVerific(direction): # sirve para verificar datos
 	try:
-		if int(direction)==1:
+		if int(direction)==1: #si la dirección es igual a 1, la dirección toma el valor de "Peru, Lima" siguiendo así con los otros
 			direction="Peru, Lima"
-			return direction
+			return direction #Luego retorna el valor
 		elif int(direction)==2:
 			direction="Peru, Cusco"
 			return direction
@@ -154,69 +154,74 @@ def cityVerific(direction):
 			return direction
 		else:
 			direction="Peru, Lima"
-			return direction
+			return direction #si ninguno coincide, entonces solo retorna Peru, Lima
 	except Exception as e:
-		return "Peru, Lima"
+		return "Peru, Lima" # si el valor introducido es erroneo, como colocar algo extraño, retorna "Peru, Lima"
 
-def account_signup(user_name, user_surname, email, password, telephone, direction, age):
+def account_signup(user_name, user_surname, email, password, telephone, direction, age): #para registrarse
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
-	print(numerRETURN(email), "ok")
+	#si los valores de un numero es None significa que no existe un numero, lo que es admitible, pues un numero debe ser unico
+	# si el email no existe en la base de datos, es admitible, por que debe ser unico
+	# luego con ayuda de regex o expresiones regulares, si los otros datos son diferentes de "None", son correctos y admitibles
+	#Si todo el condicional retorna True, entra al condicional para insertar los datos a la base de datos
+
 	if numerRETURN(email)==None and nameRETURN(email)==None and str(nick_re.search(user_name))!="None" and str(nick_re.search(user_surname))!="None" and str(email_re.search(email))!="None" and str(password_re.search(password))!="None" and str(tel_peru_re.search(telephone))!="None" and direction!="None" and str(age_re.search(age))!="None":
-		user_id=random.randint(99999,99999999)
+		user_id=random.randint(99999,99999999) #crea un id con ayuda de un modulo random
 		content=[(user_name), (user_surname), (email), (password), (telephone), (cityVerific(direction)), (age), (user_id)]
 		cursor.execute("INSERT or IGNORE INTO accounts (user_name,user_surname, email, password, phone, direction, age, user_id) VALUES (?,?,?,?,?,?,?,?)",content)
-		db_app.commit()
-		db_app.close()
-		return True
+		db_app.commit() #guarda
+		db_app.close() #cierra la conexión
+		return True #retorna true
 	else:
-		db_app.close()
+		db_app.close() #si el condicional no es correcto, por que la persona no ingresó datos correctos, cierra la conexión, y retorna false
 		return False
 
-def nameRETURN(correo):
+def nameRETURN(correo): #esto solo retorna el nombre
+	db_app = sqlite3.connect("database.db", check_same_thread=False)
+	cursor = db_app.cursor()
+	account_data=cursor.execute('SELECT * FROM accounts ORDER BY _rowid_').fetchall()
+	for i in account_data: #en un bucle recorre los datos para verificar si el email es igual a uno que exisa
+		if correo==i[2]: #si existe
+			db_app.close() #cierra la base de datos
+			return i[0]  #retorna el nombre a la cual el correo esté conectado
+		else: #si no, no retorna None
+			db_app.close()
+			pass
+
+def numerRETURN(correo): #Basicamente es el mismo al de arriba
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	account_data=cursor.execute('SELECT * FROM accounts ORDER BY _rowid_').fetchall()
 	for i in account_data:
-		if correo==i[2]:
+		if correo==i[2]: # corre el bucle y determina si el correo es igual a uno que exista
 			db_app.close()
-			return i[0]
+			return i[4] #retorna el numero
 		else:
-			pass
-	db_app.close()
-
-def numerRETURN(correo):
-	db_app = sqlite3.connect("database.db", check_same_thread=False)
-	cursor = db_app.cursor()
-	account_data=cursor.execute('SELECT * FROM accounts ORDER BY _rowid_').fetchall()
-	for i in account_data:
-		if correo==i[2]:
-			db_app.close()
-			return i[4]
-		else:
+			db_app.close() #si no existe retorna None y cierra la conexión
 			pass
 
 
-def accountUPDATE(data_update, code_user, data_modific, password): #return True or None
+def accountUPDATE(data_update, code_user, data_modific, password): #esto es para actualizar los datos de la cuenta
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
-	for i in return_all():
-		if  i[3]==str(password) and str(i[6])==str(code_user):
-			cursor.execute("UPDATE accounts set {0}='{1}' where user_id='{2}'".format(data_modific, data_update, code_user))
+	for i in return_all(): #recorre los datos
+		if  i[3]==str(password) and str(i[6])==str(code_user): #verifica si el correo y la contraseña son correctos
+			cursor.execute("UPDATE accounts set {0}='{1}' where user_id='{2}'".format(data_modific, data_update, code_user)) #modifica los datos
 			db_app.commit()
 			db_app.close()
 			return True
 
 	db_app.close()
 
-def registerComputer():
+def registerComputer(): #recorre los datos de la tabla computerDB
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM computerDB').fetchall()
 	db_app.close()
 	return datos
 
-def registerIncidents():
+def registerIncidents(): #igual que el de arriba pero con incidentsDB
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM incidentsDB').fetchall()
@@ -224,7 +229,7 @@ def registerIncidents():
 	return datos
 
 
-def showComputer():
+def showComputer(): #igual que el de arriba
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM computerDB').fetchall()
@@ -232,7 +237,7 @@ def showComputer():
 	return datos
 
 
-def showIncidents():
+def showIncidents(): #igual que el de arriba
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM incidentsDB').fetchall()
@@ -240,14 +245,14 @@ def showIncidents():
 	return datos
 
 
-def showMove():
+def showMove(): #igual que el de arriba
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM moveDB').fetchall()
 	db_app.close()
 	return datos
 
-def showImage():
+def showImage(): #igual que el de arriba
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	datos=cursor.execute('SELECT * FROM imagesDB').fetchall()
@@ -255,16 +260,16 @@ def showImage():
 	return datos
 
 
-def deleteAccounts(email, password): #true is ok # false is error with regular expretion # None is login failed
+def deleteAccounts(email, password): # elimina una cuenta
 	db_app = sqlite3.connect("database.db", check_same_thread=False)
 	cursor = db_app.cursor()
 	for i in return_all():
-		if str(email_re.search(email))!="None" and str(password_re.search(password))!="None":
+		if str(email_re.search(email))!="None" and str(password_re.search(password))!="None": #verifica si el correo y el usuario existan
 			if email==i[2] and password == i[3]: #if email is a email
-				datos=cursor.execute('DELETE FROM accounts WHERE email = "{0}";'.format(email))
+				datos=cursor.execute('DELETE FROM accounts WHERE email = "{0}";'.format(email)) # si existe, lo elimina
 				db_app.commit()
 				db_app.close()
 				return True
-		else: #email or password is incorrect
+		else:
 			db_app.close()
 			return False 
--- init.py
+++ init.py
@@ -20,95 +20,78 @@ socketio = SocketIO(app) #socketio
 app.secret_key="secret_key" #un código secreto, para realizar peticiones GET, POST
 
 
-@app.route("/", methods=["GET","POST"]) #Crea un link
+@app.route("/", methods=["GET","POST"]) #Crea un link y admite los metodos get y post
 def Index(): # Se ejecuta lo que esta adentro cuando entran al link
 	return render_template("index.html") #Se ejecuta para visualizar la página.
 
 
-@app.route("/login", methods=["GET","POST"])
-def login():
-	if 'emailXZK' in session:
-		usuario=nameRETURN(escape(session["emailXZK"]))
-		return render_template("home/home.html", user_name=usuario)
+@app.route("/login", methods=["GET","POST"])#Crea un link y admite los metodos get y post
+def login(): #si entran al link /login ejecuta el contenido de la función 
+	if 'emailXZK' in session: #verifica si existe un coockie
+		usuario=nameRETURN(escape(session["emailXZK"])) #si existe, usuario toma valor del coockie que es un nombre de usuario
+		return render_template("home/home.html", user_name=usuario) #muestra la pagina html y envía una variable en el html
 	else:
-		if request.method == "POST":
-			email=request.form.get('email');
-			password=request.form.get('password');
-			if password != None or email != None:
-				AL=account_login(email=email, password=password)
-				print(AL)
-				print("-------------------------")
-				if AL == None:
+		if request.method == "POST": # si no existe el coockie, este condicional espera a que se envien metodos http post
+			email=request.form.get('email'); #si se envian entra al condicional, obteniendo datos del <form>
+			password=request.form.get('password'); #obtiene datos del email y password
+			if password != None or email != None: #este condicional verifica si password y email son diferentes de None, si lo son, entra al condicional
+				AL=account_login(email=email, password=password) #envía datos a la base de datos, la función account_login en database.py para logearse
+				if AL == None: # si la función retorna None simplemente renderiza nuevamente la pagina del login con un mensaje que dice la cuenta no existe
 					return render_template("login/login.html", style_warning="text-warning text-red", text_warning="La cuenta no existe")
-				elif AL == True and AL!=None:
+				elif AL == True and AL!=None: #si es correcto, crea un coockie que es como una variable que toma valor el email
 						session['emailXZK']=email
 
-						if "emailXZK" in session:
+						if "emailXZK" in session: #si el coockie existe, osea que si se ha creado, nos envía a la función home redireccionandonos al link
 							return redirect('/home')
-						else:
+						else: #si no existe el coockie renderiza nuevamente el login con otro mensaje
 							return render_template("login/login.html", style_warning="text-warning text-red", text_warning="Ocurrió un error con coockies al logearse.")
-				elif AL!=True and AL!=None:
+				elif AL!=True and AL!=None: #si al es diferente de True o None renderiza el login por que ha ocurrido un error inesperado
 					return render_template("login/login.html", style_warning="text-warning text-white", text_warning="Ocurrió un error al logearse.")
-		return render_template("login/login.html")
+		return render_template("login/login.html") #saliendo fuera del condicional de los metodos http, renderiza el login
 
-@app.route("/signup", methods=["GET","POST"])
-def signup():
-	if request.method == "POST":
+@app.route("/signup", methods=["GET","POST"])#Crea un link y admite los metodos get y post
+def signup(): 
+	if request.method == "POST": #espera a que se envíe el metodo http post
 
-		user_name=request.form.get('user_name');
-		user_surname=request.form.get('user_surname');
+		user_name=request.form.get('user_name'); #obtiene valor de los formularios, en este caso  el name del input del form que tiene como valor user_name
+		user_surname=request.form.get('user_surname'); #lo mismo para los otros
 		email=request.form.get('email');
 		password=request.form.get('password');
 		telephone=request.form.get('telephone');
 		direction=request.form.get('direction');
 		age=request.form.get('age');
 
+		#este condicional solo verifica que los datos no estén vacios, si no lo están, retorna True y accede al condicional
+		#Si crees que esto solo se resuelve con js, estás equivocado, por que el js, se puede bloquear o simplemente cambiar valores
+
 		if user_name!=None or user_surname!=None or email!=None or password!=None or telephone!=None or direction!=None or age!=None:
 			
-			signup=account_signup(user_name, user_surname, email, password, telephone, direction, age)
-			if signup==True:
+			signup=account_signup(user_name, user_surname, email, password, telephone, direction, age) #envía los datos a la función account_signup en database.py
+			if signup==True: #si signup  la función retorna True, accede a este condicional
+				#renderiza el login con un mensaje
 				return render_template("login/login.html", style_warning="text-warning text-green", text_warning="¡Gracias por suscribirte!.")
-			else:
+			else: #si signup retorna False o None, renderiza el signup
 				return render_template("signup/signup.html", style_warning="text-warning text-red", text_warning="La cuenta no se pudo crear")
 
 	return render_template("signup/signup.html", style_warning="text-warning text-white", text_warning="Registrarse")
+	#fuera del condicional del metodo http, GET POST, renderiza signup
 
-@app.route("/home", methods=["GET","POST"])
-def home():
-	if 'emailXZK' in session:
-		usuario=nameRETURN(escape(session["emailXZK"]))
-		return render_template("home/home.html", user_name=usuario)
+@app.route("/home", methods=["GET","POST"])#Crea un link y admite los metodos get y post
+def home(): 
+	if 'emailXZK' in session: #comprueba si existe un coockie
+		usuario=nameRETURN(escape(session["emailXZK"])) # obtiene el nombre del usuario utilizando la coockie y una función
+		return render_template("home/home.html", user_name=usuario) #renderiza home con el nombre de usuario 
 	else:
-		return redirect("/login")
-
-@app.route('/home', methods=["GET", "POST"])
-def chat():
-	if "emailXZK" in session:
-		
-		return render_template("/home/home.html", usuario=usuario)
-	else:
-		return redirect('/')
+		return redirect("/login") #si no existe coockie, nos envía al login para logearnos
 
 
-@app.route("/logout")
+@app.route("/logout")#si acceden a este link ejecuta la función logout eliminando los coockies
 def logout():
 	if "emailXZK" in session:
 		session.pop("emailXZK", None)
 		return redirect("/login")
 	else:
 		return redirect("/login")
-
-
-
-
-
-@socketio.on('messageSER')
-def message(msg):
-	socketio.emit('messageCLI',msg, broascast=True)
-
-@socketio.on('messageJOIN')
-def CLIjoin(msg):
-	socketio.emit('CLICREATEuser',msg, broascast=True)
 
 
 
